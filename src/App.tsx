4608938c6f6af03d8db0c9fa2229fac8c7325fc9
Hmazah <alhamza2012@gmail.com>
2024-01-31 10:00:00 +0200

    add  flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,12 @@ function App() {
 
 
       <body>
-      <div className="w-full flex flex-row justify-center h-screen bg-red-500">
-  <div className="flex-1 bg-blue-500">01</div>
-  <div className="flex-1 bg-green-500">02</div>
-  <div className="flex-1 bg-yellow-500">03</div>
+      <div className="flex justify-center  h-screen w-screen m-auto bg-red-500">
+  <div className="flex flex-col gap-1">
+    <div className="w-screen h-1/4 bg-blue-500 text-white text-center"><span>sp</span> </div>
+    <div className="w-screen h-2/4 bg-green-500 text-white text-center"><span>sp</span></div>
+    <div className="w-screen h-1/4 bg-yellow-500 text-black text-center"><span>sp</span></div>
+  </div>
 </div>
 
 
