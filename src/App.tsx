94ae1dd3ef614ec4050dc94e2c38645ba46628e4
Hmazah <alhamza2012@gmail.com>
2024-02-08 10:00:00 +0200

    add  pluse icon animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,56 @@
-  import { useState } from 'react'
-  import { PlusIcon } from '@heroicons/react/24/outline'
-  import './App.css'
+import { useState } from 'react'
+import { PlusIcon } from '@heroicons/react/24/outline'
+import './App.css'
 
-  function App() {
-
-
-    return (
-      <>
-
-        <head>
-          <title>Travel Agency</title>
-          <meta name="color-scheme" content="light dark" />
-        </head>
+function App() {
 
 
-        <body>
-          
-          <div className="flex justify-center  h-screen w-screen m-auto bg-red-500">
-            <div className="flex flex-col gap-0">
+  return (
+    <>
 
-              <div className="w-screen grid gap-0 lg:grid-cols-2 sm:grid-cols-1    h-1/4 bg-blue-500 text-white text-center">
+      <head>
+        <title>Travel Agency</title>
+        <meta name="color-scheme" content="light dark" />
+      </head>
+
+
+      <body>
+
+        <div className="flex justify-center  h-screen w-screen m-auto bg-red-500">
+          <div className="flex flex-col gap-0">
+
+            <div className="w-screen grid gap-0 lg:grid-cols-2 sm:grid-cols-1    h-1/4 bg-blue-500 text-white text-center">
 
               <div className="flex justify-center items-center bg-yellow-500">
-                
-                <PlusIcon className="w-10 h-10 border  border-r-4 border-indigo-500 hover:bg-fuchsia-600" />
-              </div>
-              <div className="bg-purple-500">02</div>
-              
-            
-             
+                <span className="border w-1/2 h-2/4  relative border-r-5 border-green-500  rounded-lg drop-shadow-xs
+                               ">
+
+                  <h1>Add</h1>
+                  <PlusIcon className=" transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 absolute top-0 left-0  float-right w-10 h-10 border  border-r-4 border-indigo-500 hover:bg-fuchsia-600" />
+                </span>
 
               </div>
-              <div className="w-screen h-2/4 bg-green-500 text-white text-center"><span>sp</span></div>
-              <div className="w-screen h-1/4 bg-yellow-500 text-black text-center"><span>sp</span></div>
+              <div className="bg-purple-500">02</div>
 
 
 
 
             </div>
+            <div className="w-screen h-2/4 bg-green-500 text-white text-center"><span>sp</span></div>
+            <div className="w-screen h-1/4 bg-yellow-500 text-black text-center"><span>sp</span></div>
+
+
+
+
           </div>
+        </div>
 
 
-        </body>
+      </body>
 
-      </>
+    </>
 
-    )
-  }
+  )
+}
 
-  export default App
+export default App
