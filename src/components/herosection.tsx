04bba07ecdacd379065bacb2e8934f159bfd1a52
Hmazah <alhamza2012@gmail.com>
2024-02-22 10:00:00 +0200

    edit  hero button

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -4,12 +4,14 @@ import { motion } from "framer-motion";
 
 const HeroSection = () => {
     return (
-        <div className="grid lg:grid-cols-2 sm:grid-cols-1  w-screen  min-h-full  text-center  ">
-            <motion.div className="w-full h-full p-10 text-center bg-red-500">
+        <div className="grid lg:grid-cols-2 sm:grid-cols-1  w-screen h-screen text-center m-0 gap-0 " >
+         
+            <motion.div className="max-h-1/2 p-10  flex  flex-col justify-center items-center gap-10    bg-red-500">
                 <h1 className="text-4xl font-bold hover:text-blue-500">Hero Section</h1>
-                <button className="bg-blue-500 w-1/4 rounded-md py-10 text-white p-2">Click me</button>
+                <button className="bg-blue-500 w-40  rounded-lg py-10 text-white p-2">Click me</button>
             </motion.div>
-            <motion.div className="w-full h-full bg-blue-500">
+
+            <motion.div className="max-h-1/2 flex flex-col justify-center items-center bg-blue-500">
                 <h1>Hero Section</h1>
             </motion.div>
         </div>
