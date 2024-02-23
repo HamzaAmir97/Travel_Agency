0555b1e4bbb8d2ee524ffa6663dc75735b56bcec
Hmazah <alhamza2012@gmail.com>
2024-02-23 10:00:00 +0200

    edit  hero button

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -8,11 +8,12 @@ const HeroSection = () => {
          
             <motion.div className="max-h-1/2 p-10  flex  flex-col justify-center items-center gap-10    bg-red-500">
                 <h1 className="text-4xl font-bold hover:text-blue-500">Hero Section</h1>
-                <button className="bg-blue-500 w-40  rounded-lg py-10 text-white p-2">Click me</button>
+                <button className="bg-blue-500 w-40  rounded-lg  text-white p-2">Click me</button>
             </motion.div>
 
-            <motion.div className="max-h-1/2 flex flex-col justify-center items-center bg-blue-500">
-                <h1>Hero Section</h1>
+            <motion.div className="max-h-1/2  h-full w-full flex flex-col  justify-center items-center bg-blue-500">
+                <img src={"1.png"} alt="piza"  className=" bg-red-500  w-1/2  h-90 rotate-12 object-cover"/> 
+
             </motion.div>
         </div>
 
